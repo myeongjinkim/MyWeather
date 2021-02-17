aa1deb71adbff8694d1ac92a0226299d8251c167
myeongjinkim <kiakd2@naver.com>
2021-02-18 03:00:14 +0900

    Change 'Call' to 'Observable' using RxKotlin

--- app/src/main/java/com/example/myweather/MainActivity.kt
+++ app/src/main/java/com/example/myweather/MainActivity.kt
@@ -7,10 +7,14 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myweather.BuildConfig.OpenWeatherKey
 import com.example.myweather.databinding.ActivityMainBinding
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 
@@ -18,52 +22,42 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var myApi: MyApi
     private val myKey= OpenWeatherKey //openweather api key
+    var disposable: Disposable? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+
         val retrofit = Retrofit.Builder()
-            .baseUrl("https://api.openweathermap.org")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+                .baseUrl("https://api.openweathermap.org")
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
         myApi =retrofit.create(MyApi::class.java)
 
     }
     fun myWeather(view: View){
-        lateinit var getCall: Call<MyWeather>
-        if((view as Button).text.equals("서울")){
-            getCall = myApi.getWeather("Seoul",myKey)
-        }else if((view).text.equals("인천")){
-            getCall = myApi.getWeather("Incheon",myKey)
-        }else if((view).text.equals("부산")){
-            getCall = myApi.getWeather("Busan",myKey)
-        }
+        disposable = myApi.getWeather((view as Button).text as String,myKey)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { result ->
+                            binding.textView1.setText("도시 : ${(view).text}")
+                            binding.textView2.setText("날씨 : ${result.weather?.get(0)?.main} - ${result.weather?.get(0)?.description}")
+                            binding.textView3.setText("온도 : ${result.main?.temp}")
+                            binding.textView4.setText("체감 온도 : ${result.main?.feels_like}")
+                            binding.textView5.setText("최저 온도 : ${result.main?.temp_min}")
+                            binding.textView6.setText("최고 온도 : ${result.main?.temp_max}")
+                        },
+                        { error -> Log.d("Error Msg:", error.message.toString()) }
+                )
 
-
-        getCall.enqueue(object : Callback<MyWeather> {
-
-            override fun onResponse(call: Call<MyWeather>, response: Response<MyWeather>) {
-                if (response.isSuccessful()) {
-                    var item = response.body()
-
-                    binding.textView1.setText("도시 : ${(view).text}")
-                    binding.textView2.setText("날씨 : ${item?.weather?.get(0)?.main} - ${item?.weather?.get(0)?.description}")
-                    binding.textView3.setText("온도 : ${item?.main?.temp}")
-                    binding.textView4.setText("체감 온도 : ${item?.main?.feels_like}")
-                    binding.textView5.setText("최저 온도 : ${item?.main?.temp_min}")
-                    binding.textView6.setText("최고 온도 : ${item?.main?.temp_max}")
-
-                } else {
-                    Log.d("aa", "Status Code : " + response.code())
-                }
-            }
-
-            override fun onFailure(call: Call<MyWeather>, t: Throwable) {
-                Log.d("aa", "Fail msg : " + t.message)
-            }
-        })
-
+    }
+    override fun onPause() {
+        super.onPause()
+        disposable?.dispose()
     }
 }
--- app/src/main/java/com/example/myweather/MyApi.kt
+++ app/src/main/java/com/example/myweather/MyApi.kt
@@ -1,12 +1,11 @@
 package com.example.myweather
 
-import retrofit2.Call
+import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-
 interface MyApi {
     @GET("/data/2.5/weather")
-    fun getWeather(@Query("q") myCity: String, @Query("appid") myKey: String): Call<MyWeather>
+    fun getWeather(@Query("q") myCity: String, @Query("appid") myKey: String): Observable<MyWeather>
 
 }
